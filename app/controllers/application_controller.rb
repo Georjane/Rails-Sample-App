b39b754d92fd9457aff2f9b63971913b90fd905c
Georjane <pzgeorjane@gmail.com>
2020-10-05 15:49:52 +0000

    Edit hello action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
   def hello
-    render text: "hello, world!"
+    render html: "hello, world!"
   end
 end
