aecfeb0d0ea96f9b9fd97e98811ff2ec8209a84f
Georjane <pzgeorjane@gmail.com>
2020-10-07 08:54:48 +0000

    Changed routes from static_pages/home to home path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-  
-  get 'static_pages/about'
-
   root 'static_pages#home'
-  
-  get 'static_pages/contact'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
