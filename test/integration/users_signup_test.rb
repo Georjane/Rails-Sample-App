0d42dbc05b9ffb80bc752ec3d315fed6308f444c
Georjane <pzgeorjane@gmail.com>
2020-10-08 14:33:24 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,10 +15,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" } }
+      post users_path, params: { user: { name: "Example User", email: "user@example.com", password: "example", password_confirmation: "example" } }
     end
     follow_redirect!
     assert_template 'users/show'
-    # assert_not flash.danger
+    assert is_logged_in?
+    # assert_not flash.empty?
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,11 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
+  
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 
   # Add more helper methods to be used by all tests here...
 end
