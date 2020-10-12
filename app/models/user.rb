7526a8d7e9b7b435ba62c86e0d066288f717b7cf
Georjane <pzgeorjane@gmail.com>
2020-10-12 10:14:16 +0000

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  
   # ensuring uniqueness by downcasing emails before saving before_save { email.downcase! }
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
@@ -8,8 +10,32 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ?  BCrypt::Engine::MIN_COST :  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ?  BCrypt::Engine::MIN_COST :  BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+    
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
